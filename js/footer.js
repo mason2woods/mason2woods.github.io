e2a10c89a09079e64fbb02a51e24403d7bf50e04
Mason Woods <msg1027@hotmail.com>
2019-09-25 12:45:33 +0800

    first push

---
+++ js/footer.js
@@ -0,0 +1,9 @@
+﻿document.write ("<div class='footer'>");
+    document.write ("<div class='footer_nav'>");
+        document.write ("<a href='http://cryptoline.660v.com/' target='_blank'>加密在线</a> 丨 <a href='#'>网站简介</a> 丨 <a href='#'>广告服务</a> 丨 <a href='#'>新站提交</a> 丨 ");
+        document.write ("<a href='#'>网站留言</a> 丨 <a href='#'>联系我们</a>");
+    document.write ("</div>");
+    document.write ("<div class='fengexian'></div>");
+    document.write ("<p class='statement'>声明：数字货币价格波动大，投资交易需谨慎，本站将不会对可能引起的任何损失负责，加密在线网对此保留所有权利！</p>");
+    document.write ("<p class='copyright'>Copyright © 2019 cryptoline.660v.com. All Rights Reserved </p>");
+document.write ("</div>")
